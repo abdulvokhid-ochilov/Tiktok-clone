86671ad54da77f6354d2c487196af6f089b27a3f
Abdulvokhid Ochilov <ochilov2050@gmail.com>
2022-08-05 21:51:09 +0900

    utils

---
+++ components/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
